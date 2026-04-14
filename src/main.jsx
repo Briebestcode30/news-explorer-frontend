532f6b5e657cfdd479905ed61ebe8a8621736256
Briebestcode30 <brieanaharris033@penguin.com>
2026-04-14 13:06:33 -0400

    updated missing comma in package.json also in main.jsx fixed padding bottom on saved-news__content

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/news-explorer-frontend">
+      {" "}
       <App />
     </BrowserRouter>
   </React.StrictMode>,
